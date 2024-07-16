bd8004b936edc4d32fefffb620d3f9ef9795157f
ADITYA SINHA <119526636+ADICODZZ@users.noreply.github.com>
2024-07-16 20:35:33 +0530

    Update index.js

--- SERVER/index.js
+++ SERVER/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"https://edu-tube1.vercel.app",
+        origin:["https://edu-tube1.vercel.app"],
         Credential:true,
     })
 )
